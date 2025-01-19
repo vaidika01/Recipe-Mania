6fdd4ebfa93b45f43cbba26a13f6008e8e614a41
Vaidika <136914466+vaidika01@users.noreply.github.com>
2025-01-20 01:52:12 +0530

    Update axiosInstance.js

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.DATABASE_URL || "https://recipe-mania-backend.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://recipe-mania-backend.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use(
